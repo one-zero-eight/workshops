66773b63a42913e00f255061be2af40aa574160d
tomatoCoderq <xasanFN@mail.ru>
2025-06-20 10:24:09 +0300

    fix:from ViewUserScheme removed redundant fields

--- src/modules/users/schemes.py
+++ src/modules/users/schemes.py
@@ -9,17 +9,7 @@ class CreateUserScheme(SQLModel):
     innohassle_id: str
 
 
-class ViewUserSchemeOld(SQLModel):
-    id: str
-    innohassle_id: str
-    email: str
-    name: str | None
-    role: UserRole
-
-
 class ViewUserScheme(SQLModel):
     id: str
     innohassle_id: str
-    email: str
-    name: str | None
     role: UserRole
