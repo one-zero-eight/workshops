09ae8f92e51bbdfd59f05393d88a8b886a7a0f94
Ilnur <ramazanovilnur2006@gmail.com>
2025-06-29 23:36:38 +0300

    30-fix-bugs-with-workshops

--- src/modules/workshops/enums.py
+++ src/modules/workshops/enums.py
@@ -4,12 +4,13 @@ from enum import Enum
 class CheckInEnum(Enum):
     SUCCESS = "Success"
     NOT_ACTIVE = "Workshop is not active"
-    ALREADY_CHECKED_IN = "You already checked in this workshop"
+    ALREADY_CHECKED_IN = "You already checked in for this workshop"
     WORKSHOP_DOES_NOT_EXIST = "Workshop does not exist"
     NO_PLACES = "No places available"
-    INVALID_TIME = "You can not check in this workshop now"
+    INVALID_TIME = "You cannot check in for this workshop yet"
+    TIME_IS_OVER = "The time for this workshop is over"
     OVERLAPPING_WORKSHOPS = "You have overlapping workshops"
-    CHECK_IN_DOES_NOT_EXIST = "You did not check in this workshop"
+    CHECK_IN_DOES_NOT_EXIST = "You did not check in for this workshop"
 
 
 class WorkshopEnum(Enum):
