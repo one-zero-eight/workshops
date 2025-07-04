55a31deeab88b987f1b18a821e81512059c17adc
tomatoCoderq <xasanFN@mail.ru>
2025-07-04 11:18:28 +0300

    feat: blocking change_role endpoint in prod

--- src/modules/users/routes.py
+++ src/modules/users/routes.py
@@ -9,6 +9,7 @@ from src.modules.users.dependencies import UsersRepositoryDep
 
 from src.api.dependencies import CurrentUserIdDep
 from src.logging import logger
+from src.config import settings
 
 
 load_dotenv()
@@ -50,25 +51,29 @@ async def get_my_checkins(
     return [ReadWorkshopScheme.model_validate(workshop) for workshop in workshops]
 
 
-# TODO: REMOVE IN THE FUTURE
-
-
 @router.post(
     "/change_role",
     responses={
         status.HTTP_200_OK: {"description": "Changed role succesfully"},
-        status.HTTP_401_UNAUTHORIZED: {"description": "Not authenticated"},
-        status.HTTP_404_NOT_FOUND: {"description": "Failed during changing role"},
+        status.HTTP_403_FORBIDDEN: {
+            "description": "Changing role is not allowed in production"
+        },
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {"description": "User not found"},
     },
 )
 async def change_role(
     role: str, users_repo: UsersRepositoryDep, user_id: CurrentUserIdDep
 ):
+    if settings.is_prod == "True":
+        raise HTTPException(
+            status_code=403, detail="Changing role is not allowed in production"
+        )
+
     user = await users_repo.read_by_id(user_id)  # type: ignore
     if user is None:
         raise HTTPException(status_code=500, detail="User not found")
 
     userChanged = await users_repo.change_role_of_user(user.id, role)
     if not userChanged:
-        raise HTTPException(status_code=404, detail="Role not changed")
+        raise HTTPException(status_code=500, detail="Role not changed")
     return Response(status_code=status.HTTP_200_OK)
