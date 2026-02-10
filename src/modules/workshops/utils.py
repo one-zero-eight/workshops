99d48f09719c3e910927a267d8ce9d756acf52c0
PoweredDeveloper <mikis.explores@gmail.com>
2026-02-10 22:55:47 +0300

    fix: update club id to use host names

--- src/modules/workshops/utils.py
+++ src/modules/workshops/utils.py
@@ -3,5 +3,5 @@ from src.storages.sql.models import Host, HostType
 
 
 def is_leader_of_club(user_clubs: list[Club], hosts: list[Host]):
-    club_ids = [host.id for host in hosts if host.host_type == HostType.CLUB]
+    club_ids = [host.name for host in hosts if host.host_type == HostType.CLUB]
     return any([club.id in club_ids for club in user_clubs])
